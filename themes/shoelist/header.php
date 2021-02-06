d488cdb4b00b4f6d519150d71ed6048bd6884677
Shubham Mingwal <shubhammingi@gmail.com>
2021-02-06 00:16:00 -0500

    Header developed

--- themes/shoelist/header.php
+++ themes/shoelist/header.php
@@ -26,34 +26,47 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'shoelist' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$shoelist_description = get_bloginfo( 'description', 'display' );
-			if ( $shoelist_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $shoelist_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<div class="grid-container fluid">
+			<div class="grid-x grid-margin-x">
+				<div class="cell  medium-2 small-11">
+					<div class="site-branding">
+						<?php
+						the_custom_logo();
+						if ( is_front_page() && is_home() ) :
+							?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+							<?php
+						else :
+							?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+							<?php
+						endif;
+						$shoelist_description = get_bloginfo( 'description', 'display' );
+						if ( $shoelist_description || is_customize_preview() ) :
+							?>
+							<p class="site-description"><?php echo $shoelist_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+						<?php endif; ?>
+					</div><!-- .site-branding -->
+				</div>
+				<div class="cell medium-6 small-11">
+					<nav id="site-navigation" class="main-navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'shoelist' ); ?></button>
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'menu-primary',
+								'menu_id'        => 'primary-menu',
+							)
+						);
+						?>
+					</nav><!-- #site-navigation -->
+				</div>
+				<div class="cell medium-4 small-11 search">
+					<input type="text" name="" id="searchInput" placeholder="Search here....">
+					<a href="#"><img src="profile-logo.png" alt="profile logo"></a>
+				</div>
+			</div>
+		</div>
+		
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'shoelist' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-primary',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
