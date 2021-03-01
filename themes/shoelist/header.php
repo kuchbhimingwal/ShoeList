0f1d39955acc45fa8c5809542ea74740f14321f8
Shubham Mingwal <shubhammingi@gmail.com>
2021-03-01 15:34:17 -0500

    Header upgrated with cart logo

--- themes/shoelist/header.php
+++ themes/shoelist/header.php
@@ -62,12 +62,13 @@
 					</nav><!-- #site-navigation -->
 				</div>
 				<div class="cell medium-4 small-11 search">
-				<form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
-	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'woocommerce' ); ?></label>
-	<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'woocommerce' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'woocommerce' ); ?>" />
-	<input type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'woocommerce' ); ?>" />
-	<input type="hidden" name="post_type" value="product" />
-</form>					<a href="#"><img src="http://shoelistwebsite.local/wp-content/uploads/2021/02/profile-logo.png" alt="profile logo"></a>
+					<form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
+						<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'woocommerce' ); ?></label>
+						<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'woocommerce' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'woocommerce' ); ?>" />
+						<input type="hidden" name="post_type" value="product" />
+					</form>					
+
+					<a href="http://shoelistwebsite.local/cart/"><img src="http://shoelistwebsite.local/wp-content/uploads/2021/03/cart.png" alt="profile logo"></a>
 				</div>
 			</div>
 		</div>
