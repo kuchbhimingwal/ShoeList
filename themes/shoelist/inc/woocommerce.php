9aa5ce8f87650d00276f2a29ab7db28132a2435b
Shubham Mingwal <shubhammingi@gmail.com>
2021-03-01 18:57:29 -0500

    Some style chnges to the product page

--- themes/shoelist/inc/woocommerce.php
+++ themes/shoelist/inc/woocommerce.php
@@ -8,3 +8,13 @@ add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_t
 //     echo 'Look at the amazing prize';
 // }
 // add_action( 'woocommerce_single_product_summary', 'sampletheme_woocommerce_single_product_summary_subtitle', 1);
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_output_product_data_tabs', 60 );
+add_filter( 'wc_product_sku_enabled', 'bbloomer_remove_product_page_sku' );
+  
+function bbloomer_remove_product_page_sku( $enabled ) {
+    if ( ! is_admin() && is_product() ) {
+        return false;
+    }
+    return $enabled;
+}
